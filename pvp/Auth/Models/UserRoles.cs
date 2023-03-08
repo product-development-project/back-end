41d28e4442a5f85885acb0f0ea077c84cd604de3
augis1123 <99415938+augis1123@users.noreply.github.com>
2023-03-09 01:36:25 +0200

    Padarytas

--- pvp/Auth/Models/UserRoles.cs
+++ pvp/Auth/Models/UserRoles.cs
@@ -6,6 +6,7 @@
         public const string User = nameof(User);
         public const string Company = nameof(Company);
         public const string Alll = Admin + "," + User + "," + Company;
+        public const string CompanyAndAdmin = Admin + "," + Company;
         public static readonly IReadOnlyCollection<string> All = new[] { Admin, User, Company };
     }
 }
